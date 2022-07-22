0a997c7a4a230eb0128b0727daa11bbf4fc67116
Harshitjain-22 <hjdec22@gmail.com>
2022-07-23 01:58:24 +0530

    Absolutey no idea what redux is and just followed Pizza Shop tutorial......even though the module is being depricated don't know how it woked I just removed its @depricated value from its package and it just worked.....Pizza Shop tutorial video 16

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { Provider } from "react-redux";
-// import store from "./store";
+import { Provider } from "react-redux";
+import store from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  // <Provider store={store}>
+  <Provider store={store}>
     <div>
     <App />
     </div>,
-  // </Provider>,
+   </Provider>,
   document.getElementById("root")
 );
